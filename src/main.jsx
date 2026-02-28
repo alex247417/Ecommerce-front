4ad364e67722e02576a69f8f5ff5c38843ca00df
Alecsandro <alex247417@hotmail.com>
2026-02-28 16:48:42 -0300

    fix: corrige nome dos pacotes de fonte para minusculo

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,9 @@ import '@fontsource/outfit/400.css';
 import '@fontsource/outfit/600.css';
 import '@fontsource/outfit/700.css';
 import '@fontsource/outfit/800.css';
-import '@fontsource/inter/400.css';
-import '@fontsource/inter/500.css';
+import '@fontsource/dm-sans/300.css';
+import '@fontsource/dm-sans/400.css';
+import '@fontsource/dm-sans/500.css';
 import App from './App.jsx'
 import './index.css'
 
